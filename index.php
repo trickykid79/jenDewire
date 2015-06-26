1937c4b74cc0dc9eb1c75b8b266ddfc5786fd9bc
trickykid79 <trickykid79@gmail.com>
2015-06-26 17:09:54 +1000

    Create index.php
    Front page of my portfolio site

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Steven Lee's portfolio</title>
+<link href="stylesheet/mystyle.css" rel="stylesheet" type="text/css" />
+<link href="stylesheet/default/default.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="stylesheet/light/light.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="stylesheet/dark/dark.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="stylesheet/bar/bar.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="stylesheet/nivo-slider.css" rel="stylesheet" type="text/css" media="screen"/>
+<script src="scripts/modernizr.custom.js"></script>
+<script src="scripts/modernizr.custom02.js"></script>
+</head>
+
+<body>
+	<header>
+        <div id="titleWrapper">
+        	<div id="logoAndBanner">
+            	<div id="JendewireLogo">
+                	<a href="#"><img src="images/Jen.png" width="139" height="195" alt="Jendewire Logo" /></a>
+                </div>
+                
+            </div>
+        </div>
+    </header>
+    
+    	<div id="Navigation Bar">
+        	<nav>
+            	<ul>
+                	<div class="Horizontalline">
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="gallery.html">Work Gallery</a></li>
+                        <li><a href="interest.html">Personel Interest</a></li>
+                        <li><a href="Html/GraphAPI.html">API</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </div>     
+               </ul>
+        	</nav>
+            <br class="cleaerfloat"/>
+        </div>
+        
+              <!--body has the class "cbp=spmenu-push"-->
+      <nav class ="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+      	<h3>Widget</h3>
+        <a href="about.html">Notes</a>
+        <a href="#">Calendar</a>
+        <a href="#">E-mail</a>
+        <a href="#">Analytics</a>
+        <a href="#">System settings</a>
+        <a href="#">Search</a>
+      </nav> 
+        
+       <div id="wrapper">
+		 <button id="showLeft">=</button>
+            <div class="slider-wrapper theme-default">
+                  <div id="slider" class="nivoSlider">
+                      <img src="images/telsa-applewatch.jpg" data-thumb="#" alt="" />
+                      <a href="http://unrealengine.com"><img src="images/unreal4-particleeffect.jpg" data-thumb="#" alt="" title="Unreal engine particle effects cascade" /></a>
+                      <img src="images/Therme_Vals.jpg" data-thumb="images/walle.jpg" alt="" data-transition="slideInLeft" />
+                      <img src="images/SunnySpotHolidaysBanner.png" data-thumb="#" alt="" title="#htmlcaption" />
+                  </div>
+                  <div id="htmlcaption" class="nivo-html-caption">
+                      <strong>This is a sneak peak preview of upcoming projects</strong> 
+                  </div>
+            </div>   
+      </div>
+
+
+
+	<div class="container">	
+			<div id="bl-main" class="bl-main">
+              
+				<section>
+					<div class="bl-box">
+						<h2 class="bl-icon bl-icon-about">Val Head</h2>
+					</div>
+					<div class="bl-content">
+						<h2>About Val Head</h2>
+						<p>Val Head is a designer who rocks web animation. She wrote the CSS Animations Pocket Guide, teaches CSS Animation on lynda.com, and hosts the All The Right Moves screencast. Her articles and tutorials have been published in A List Apart, 24 Ways, and net magazine. She speaks and leads workshops around the world on interface animation for the web.</p>
+						<p>
+							<a href="http://valhead.com"><strong>&laquo; Visit Val Head's Blog</strong></a> |
+							<a href="#"><strong>&laquo; Visit Steven Lee's Blog</strong></a>
+						</p>
+					</div>
+					<span class="bl-icon bl-icon-close"></span>
+				</section>
+                
+                
+                
+				<section id="bl-work-section">
+					<div class="bl-box">
+						<h2 class="bl-icon bl-icon-works">Unreal Engine</h2>
+					</div>
+					<div class="bl-content">
+						<h2>Unreal Engine 4.7 has arrived</h2>
+						<p>Unreal Engine 4 is a complete suite of game development tools made by game developers, for game developers. From 2D mobile games to console blockbusters and VR, Unreal Engine 4 gives you everything you need to start, ship, grow and stand out from the crowd.</p>
+						<ul id="bl-work-items">
+							<li data-panel="panel-1"><a href="#"><img src="images/Unreal_gallery 01.png" /></a></li>
+							<li data-panel="panel-2"><a href="#"><img src="images/Unreal_gallery 02.jpg" /></a></li>
+							<li data-panel="panel-3"><a href="#"><img src="#" /></a></li>
+							<li data-panel="panel-4"><a href="#"><img src="#" /></a></li>
+						</ul>
+					</div>
+					<span class="bl-icon bl-icon-close"></span>
+				</section>
+                
+                
+                
+                
+				<section>
+					<div class="bl-box">
+						<h2 class="bl-icon bl-icon-blog">Parrot</h2>
+					</div>
+					<div class="bl-content">
+						<h2>Register Parrot Developer Forum now</h2>
+						<article>
+							<h3>AN OPPORTUNITY FOR VIDEO GAMES PUBLISHERS & DEVELOPERS</h3>
+							<p>By registering on Ardrone.org, you will be able to download the Software Development Kit and start developing great and innovative video games.
+
+Thanks to the Open Game API we provide, your projects will change the way video games are played, both indoor and outdoor.</p>
+						</article>
+						<article>
+							<h3>And remember, you will keep 100% of the revenue sales of your AR.DRONE compatible applications!
+A GAME CHANGER FOR THE WAY VIDEO GAMES ARE PLAYED INDOOR & OUTDOOR</h3>
+							<p>While the iPhone will display the HUD, controls, animations and sounds, the Parrot AR.DRONE will fly around you. Stop moving your iPhone and the AR.DRONE will stay put, steady, balancing its engines to fly as well as a dragonfly.
+The AR.DRONE can play solo games with Augmented Reality, relying on 2D or 3D gametags in the real world to create virtual tracks or display virtual ennemies.</p>
+						</article>
+						<article>
+							<h3>ARDrone API: SDK 1.8 just released!</h3>
+							<p>SDK 1.8 is available for download now</p>
+						</article>
+					</div>
+					<span class="bl-icon bl-icon-close"></span>
+				</section>
+                
+                
+                
+                
+				<section>
+					<div class="bl-box">
+						<h2 class="bl-icon bl-icon-contact">Sunny Spot Holidays</h2>
+					</div>
+					<div class="bl-content">
+						<h2>Sunny Spot Holidays Android App</h2>
+						<p>Android App will be launching in Google Play Store soon.</p>
+                            <ul id="bl-work-items">
+                                <li data-panel="panel-1"><a href="#"><img src="images/SunnySpotHolidaysBanner.png" /></a></li>
+                                <li data-panel="panel-2"><a href="#"><img src="images/SunnySpotHolidaysBanner01.png" /></a></li>
+                                <li data-panel="panel-2"><a href="#"><img src="images/SunnySpotHolidaysBanner02.png" /></a></li>
+                                <li data-panel="panel-2"><a href="#"><img src="images/SunnySpotHolidaysBanner03.png" /></a></li>
+                            </ul>
+					</div>
+					<span class="bl-icon bl-icon-close"></span>
+				</section>
+     </div>
+     </div>
+                
+         
+     <div id="panoroma-wrapper">
+     	<div id="panorama">
+        	<img id="bottomPhoto" src="images/aphex-twin.jpg"  height="400"alt="Everything is remix.jpg" />
+     	</div>
+     </div>
+            
+      
+     <footer>
+        	<div id="footerWrapper">
+            	<ul>
+                          <li><a href="about.html">About author</a></li>
+                          <li><a href="#">News</a></li>
+                          <li><a href="#">Site Map</a></li>
+                          <li><a href="#">Contact</a></li>
+           
+                </ul>
+                 <br />
+                 <br />
+                 <br />
+                 <br />
+                 <br />
+                 
+                <hr />
+                <p id="copyright">2015 | This site is sole property of Jendewire Pty Ltd Copyright@2015 All Right Reserved</p>
+            </div>
+    </footer>
+        <script type="text/javascript" src="scripts/jquery-1.9.0.min.js"></script>
+		<script type="text/javascript" src="scripts/jquery.nivo.slider.js"></script>
+        <script type="text/javascript">
+        $(window).load(function() {
+            $('#slider').nivoSlider();
+        });
+        </script>
+
+        <script src="scripts/classie.js"></script>
+        <script src="scripts/myscript.js"></script>
+            <script>
+                $(function() {
+                    Boxlayout.init();
+                });
+            </script>
+        
+        
+</body>
+</html>
